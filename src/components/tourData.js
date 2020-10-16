3e949bfc5ac2e8cbe9077c654d05231d84a6cf2a
Steven <jwan0061@gmail.com>
2020-10-17 00:39:21 +1100

    tour list project

---
+++ src/components/tourData.js
@@ -0,0 +1,30 @@
+export const tourData = [
+    {
+        id: 1,
+        city: "Melbourne",
+        img: "https://images.pexels.com/photos/3626248/pexels-photo-3626248.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        name: "Melbourne City Tower",
+        info: "Melbourne is home to many of Australia's best-known landmarks, such as the Melbourne Cricket Ground, the National Gallery of Victoria and the World Heritage-listed Royal Exhibition Building."
+    },
+    {
+        id: 2,
+        city: "Sydney",
+        img: "https://images.pexels.com/photos/995765/pexels-photo-995765.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        name: "Sydney Opera House",
+        info: "Located on Australia's east coast, extends about 70 km (43.5 mi) on its periphery towards the Blue Mountains to the west, Hawkesbury to the north, the Royal National Park to the south and Macarthur to the south-west."
+    },
+    {
+        id: 3,
+        city: "Brisbane",
+        img: "https://images.pexels.com/photos/416056/pexels-photo-416056.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        name: "Brisbane River",
+        info: "Brisbane (/ˈbrɪzbən/ (About this soundlisten) BRIZ-bən[9]) is the capital of and the most populated city in the Australian state of Queensland,[10] and the third most populous city in Australia. "
+    },
+    {
+        id: 4,
+        city: "Queensland",
+        img: "https://images.pexels.com/photos/1645863/pexels-photo-1645863.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        name: "Queensland Sea",
+        info: "Queensland, officially the State of Queensland) is a state situated in northeastern Australia, and is the second-largest and third-most populous Australian state."
+    }
+];
